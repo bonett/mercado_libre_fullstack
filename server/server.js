8db1102e47a445792a075f2398b3709c2c23471c
wilfrido bonett <wbonett10@gmail.com>
2021-09-07 21:09:23 -0500

    update

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const app = express();
 const cors = require("cors");
 
 const itemsRouter = require("./src/routes/items");
-const apiPort = 5000;
 
+app.set("port", process.env.PORT || 5000);
 app.use(cors());
 app.use(express.json());
 app.use("/items", itemsRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+app.listen(app.get("port"), () =>
+  console.log(`Server running on port ${app.get("port")}`)
+);
